fcc17bc073f899004bcb24a146dbf388afbc61e6
JOELjacob2523 <Shlomejacob@gmail.com>
2023-05-07 12:41:50 -0400

    add css

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const CONFIG = require('./config.json')
+const path = require('path');
 
 app.set('view engine', 'ejs');
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res, next) => {
     res.send('Hello World')
